da57e039e20bc4811335707f0167d38c0ca06d2a
Rucisist <andreyilse@yandex.ru>
2018-01-12 12:01:43 +0300

    Initial Commit

---
+++ RunQuestAI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunQuestAI
+//
+//  Created by Андрей Илалов on 12.01.2018.
+//  Copyright © 2018 Андрей Илалов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RunQuestAI/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RunQuestAI
+//
+//  Created by Андрей Илалов on 12.01.2018.
+//  Copyright © 2018 Андрей Илалов. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
